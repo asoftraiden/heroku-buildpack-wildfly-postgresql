6a45d32414310cfbdfc3eb97a0f031f9354ff6ea
Morten Terhart <morten.terhart@gmail.com>
2019-09-27 16:25:29 +0200

    Extract .war utilities into own script

---
+++ lib/war_utils.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env bash
+
+_find_war_with_persistence_unit() {
+    local persistenceFilePath="$1"
+
+    # Return at first match
+    local warFile
+    for warFile in "${JBOSS_HOME}"/standalone/deployments/*.war; do
+        if _war_file_contains_file "${warFile}" "${persistenceFilePath}"; then
+            echo "${warFile}"
+            return
+        fi
+    done
+}
+
+_war_file_contains_file() {
+    local zipFile="$1"
+    local file="$2"
+
+    if [ ! -f "${zipFile}" ]; then
+        return 1
+    fi
+
+    unzip -q -l "${zipFile}" "${file}" >/dev/null
+}
+
+update_file_in_war() {
+    local warFile="$1"
+    local rootPath="$2"
+    local relativeFile="$3"
+
+    if [ ! -d "${rootPath}" ]; then
+        error_return "Root path needs to be a directory: ${rootPath}"
+        return 1
+    fi
+
+    if [ ! -f "${rootPath}/${relativeFile}" ]; then
+        error_return "Relative file does not exist under root path: ${relativeFile}"
+        return 1
+    fi
+
+    if _is_relative_path "${warFile}"; then
+        warFile="$(_resolve_absolute_path "${warFile}")"
+    fi
+
+    status "Patching '${warFile##*/}' with updated persistence.xml"
+
+    (cd "${rootPath}" && zip -q --update "${warFile}" "${relativeFile}")
+}
