13d81fe4b2ab1f88a6e76eb971e89b6023aa88d4
Morten Terhart <morten.terhart@gmail.com>
2019-09-30 16:06:22 +0200

    Improved resolving absolute paths

--- lib/path_utils.sh
+++ lib/path_utils.sh
@@ -12,16 +12,16 @@ _resolve_absolute_path() {
     local path="$1"
 
     if [ -e "${path}" ]; then
-        local dir="${path%/*}"
-        dir="$(cd "${dir:-"/"}" && pwd)"
-        local file="${path##*/}"
+        local resolvedPath
+        if [ -d "${path}" ]; then
+            resolvedPath="$(cd "${path}" && pwd)"
+        elif [ -e "${path}" ]; then
+            local dir="${path%/*}"
+            dir="$(cd "${dir:-"/"}" && pwd)"
+            local file="${path##*/}"
 
-        local resolvedPath="${dir}"
-        if [ -n "${file}" ]; then
             resolvedPath="${dir}/${file}"
         fi
-
-        resolvedPath="$(shopt -s extglob; echo "${resolvedPath//\/*(\/)/\/}")"
 
         echo "${resolvedPath}"
     fi
