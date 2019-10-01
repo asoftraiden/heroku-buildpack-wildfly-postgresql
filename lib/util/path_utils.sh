3e759720e7b2809c86162cf088b329d9759222ec
Morten Terhart <morten.terhart@gmail.com>
2019-10-01 14:51:48 +0200

    Added documentation comments to path_utils.sh

--- lib/util/path_utils.sh
+++ lib/util/path_utils.sh
@@ -1,13 +1,32 @@
 #!/usr/bin/env bash
-#
-# shellcheck disable=SC2155
 
+# Determines whether a given path is relative to a directory
+# or not. The function assumes that an absolute path starts
+# with a leading '/'.
+#
+# Params:
+#   $1:  path  the path to check
+#
+# Returns:
+#   0: The path is relative
+#   1: The path is absolute
 _is_relative_path() {
     local path="$1"
 
     ! [[ "${path}" =~ ^/ ]]
 }
 
+# Resolves a given path to a normalized absolute path. The
+# normalization includes removing '.' and '..' components,
+# removing duplicate slashes as well as any trailing slashes.
+# If the specified path does not point to an existing file
+# or directory, nothing is printed to stdout.
+#
+# Params:
+#   $1:  path  the path to resolve
+#
+# Returns:
+#   stdout: the resolved path if existing
 _resolve_absolute_path() {
     local path="$1"
 
